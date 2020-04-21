9c1b232b3a3ee453cbd24ec9a4e2a3682a5f81d1
Pável Lelis <42754821+pavellelis@users.noreply.github.com>
2020-04-21 01:05:22 -0300

    Add Files via upload
    Cleaning Notebook

--- main.py
+++ main.py
@@ -32,255 +32,120 @@ black_friday = pd.read_csv("black_friday.csv")
 black_friday.info()
 
 
-# In[4]:
-
-
-black_friday[black_friday.Gender=='F'].groupby("Age").count()['User_ID']
-
-
-# In[5]:
-
-
-print( black_friday.User_ID.count(), black_friday.User_ID.nunique())
-
-
-# In[6]:
-
-
-((black_friday.Product_Category_2.isnull().sum())+(black_friday.Product_Category_3.isnull().sum()))/(12*537577 )
-
-
-# In[7]:
-
-
-print(black_friday.Product_Category_2.isnull().sum())
-print(black_friday.Product_Category_3.isnull().sum())
-
-
-# In[8]:
-
-
-black_friday.Product_Category_3.describe()
-
-
-# In[9]:
-
-
-black_friday.Product_Category_3.mode()
-
-
-# In[10]:
-
-
-black_friday.Product_Category_3.value_counts()
-
-
-# In[11]:
-
-
-df_normalizaPurchase=black_friday[~black_friday.Purchase.isnull()]
-
-
-# In[12]:
-
-
-df_normalizaPurchase.head()
-
-
-# In[13]:
-
-
-print(black_friday.Purchase.min())
-print(black_friday.Purchase.max())
-
-def normalizarPurchase(Purchase):
-    return ((Purchase - 185)/(23961-185))
-
-
-# In[15]:
-
-
-black_friday['PurchaseNormalizada']=black_friday['Purchase'].apply(normalizarPurchase)
-
-
-# In[16]:
-
-
-black_friday.PurchaseNormalizada.describe()
-
-
-# In[17]:
-
-
-black_friday.PurchaseNormalizada.mean()
-
-
-# In[18]:
-
-
-black_friday['Purchase'].describe()
-
-
-# In[34]:
-
-
-media=df_normalizaPurchase['Purchase'].mean()
-desvio=df_normalizaPurchase['Purchase'].std()
-
-def padronizaPurchase(Purchase):
-      return (Purchase-media)/desvio
-
-
-# In[35]:
-
-
-black_friday['PurchasePadronizada']=black_friday['Purchase'].apply(padronizaPurchase)
-
-
-# In[57]:
-
-
-len(black_friday['PurchasePadronizada'].loc[ (black_friday['PurchasePadronizada']<=1) &(black_friday['PurchasePadronizada']>-1) ])
-
-
-# In[59]:
-
-
-df_aux=black_friday[['Product_Category_2','Product_Category_3']]
-
-
-# In[61]:
-
-
-df_aux[df_aux.Product_Category_2.isnull()].describe()
-
-
 # ## Questão 1
 # 
 # Quantas observações e quantas colunas há no dataset? Responda no formato de uma tuple `(n_observacoes, n_colunas)`.
 
-# In[4]:
+# In[23]:
 
 
 def q1():
-    # Retorne aqui o resultado da questão 1.
-    return 537577,12
-    
+    return black_friday.shape
 
 
 # ## Questão 2
 # 
 # Há quantas mulheres com idade entre 26 e 35 anos no dataset? Responda como um único escalar.
 
-# In[5]:
+# In[24]:
 
 
 def q2():
-    # Retorne aqui o resultado da questão 2.
-    return 49348
-    
+    return len(black_friday[(black_friday['Gender'] == 'F') & (black_friday['Age'] == '26-35')])
 
 
 # ## Questão 3
 # 
 # Quantos usuários únicos há no dataset? Responda como um único escalar.
 
-# In[6]:
+# In[25]:
 
 
 def q3():
-    # Retorne aqui o resultado da questão 3.
-    return 5891
-    
+    return black_friday['User_ID'].nunique()
 
 
 # ## Questão 4
 # 
 # Quantos tipos de dados diferentes existem no dataset? Responda como um único escalar.
 
-# In[7]:
+# In[26]:
 
 
 def q4():
-    # Retorne aqui o resultado da questão 4.
-    return 3
-    
+    return black_friday.dtypes.nunique()
 
 
 # ## Questão 5
 # 
 # Qual porcentagem dos registros possui ao menos um valor null (`None`, `ǸaN` etc)? Responda como um único escalar entre 0 e 1.
 
-# In[8]:
+# In[27]:
 
 
 def q5():
-    # Retorne aqui o resultado da questão 5.
-    return 0.08375311815795691
-    
+    return float(black_friday[black_friday.isna().any(axis=1) == True].shape[0]/black_friday.shape[0])
 
 
 # ## Questão 6
 # 
 # Quantos valores null existem na variável (coluna) com o maior número de null? Responda como um único escalar.
 
-# In[9]:
+# In[28]:
 
 
 def q6():
-    # Retorne aqui o resultado da questão 6.
-    return 373299
-    
+    df_na_values = black_friday.isna()
+    df_na_count = df_na_values.apply(pd.Series.value_counts).loc[True]
+    return int(df_na_count.max())
 
 
 # ## Questão 7
 # 
 # Qual o valor mais frequente (sem contar nulls) em `Product_Category_3`? Responda como um único escalar.
 
-# In[10]:
+# In[29]:
 
 
 def q7():
-    # Retorne aqui o resultado da questão 7.
-    return 16
-    
+    return int(black_friday['Product_Category_3'].dropna().mode())
 
 
 # ## Questão 8
 # 
 # Qual a nova média da variável (coluna) `Purchase` após sua normalização? Responda como um único escalar.
 
-# In[11]:
+# In[30]:
 
 
 def q8():
-    # Retorne aqui o resultado da questão 8.
-    return 0.384794
-    
+    df = black_friday['Purchase']
+    normalized_df=(df-df.min())/(df.max()-df.min())
+    return float(normalized_df.mean())
 
 
 # ## Questão 9
 # 
 # Quantas ocorrências entre -1 e 1 inclusive existem da variáel `Purchase` após sua padronização? Responda como um único escalar.
 
-# In[12]:
+# In[31]:
 
 
 def q9():
-    # Retorne aqui o resultado da questão 9.
-    return 348631
-    
+    df_purchase = black_friday['Purchase']
+    df_purchase_zscore = (df_purchase - df_purchase.mean())/df_purchase.std(ddof=0)
+    return int(len(df_purchase_zscore[(df_purchase_zscore > -1) & (df_purchase_zscore < 1)]))
 
 
 # ## Questão 10
 # 
 # Podemos afirmar que se uma observação é null em `Product_Category_2` ela também o é em `Product_Category_3`? Responda com um bool (`True`, `False`).
 
-# In[13]:
+# In[32]:
 
 
 def q10():
-    # Retorne aqui o resultado da questão 10.
-    return True
-    
+    df = black_friday[['Product_Category_2', 'Product_Category_3']]
+    df = df[df['Product_Category_2'].isna()]
+    return df['Product_Category_2'].equals(df['Product_Category_3'])
 
